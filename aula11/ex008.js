36754e3bb5f8a5d48b6ab8e7160ff2b376141c8b
Yuri Alves dos Reis <114290965+yurialves17@users.noreply.github.com>
2023-02-22 15:40:45 -0300

    atualização

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) { // CONDIÇÃO SIMPLES!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ ex013.js
@@ -0,0 +1,31 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+console.log(diaSem)
+
+switch(diaSem) {
+    case 0: 
+        console.log('DOMINGO')
+        break
+    case 1:
+        console.log('SEGUNDA')
+        break
+    case 2: 
+        console.log('TERÇA')
+        break
+    case 3:
+        console.log('QUARTA')
+        break
+    case 4: 
+        console.log('QUINTA')
+        break
+    case 5: 
+        console.log('SEXTA')
+        break
+    case 6: 
+        console.log('SÁBADO')
+        break
+    default:
+        console.log('[ERRO] Dia Inválido!')
+        break
+}
