d36a1a2b12d7b9c1f4925962777af2b497301841
Yuri Alves dos Reis <114290965+yurialves17@users.noreply.github.com>
2023-02-25 09:59:36 -0300

    atualizações

---
+++ aula12ex/ex14/script.js
@@ -0,0 +1,25 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = document.getElementsByName('img')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 18
+
+    if(hora >= 0 && hora < 12){
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#c3cbcd'
+        msg.innerHTML = `Agora são ${hora} horas. <br>Bom dia!`
+    
+    }else if(hora >= 12 && hora <= 18){
+        img.src = 'imagens/fototarde.png'
+        document.body.style.background = '#b67468'
+        msg.innerHTML = `Agora são ${hora} horas. <br>Boa tarde!`
+    
+    }else{
+       img.src = 'fotonoite.png'
+        document.body.style.background = '#191db8'
+        msg.innerHTML = `Agora são ${hora} horas. <br>Boa noite!`
+    }
+
+}
+
