a57c39732ded08b6a8c8c458b15a65fff93eb3f9
Yuri Alves <114290965+yurialves17@users.noreply.github.com>
2023-03-10 18:10:25 -0300

    atualização

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)  // condição verificada após os comandos serem executados
+
+
+
+while (c < 6) {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+} // condição verificada enquanto for verdadeira
+
